420f874578f1a40b0389314bc77ac869cc8fd6c6
Kiarash-gh <k.gharaghozlo@yecomsoft.com>
2023-09-30 16:48:38 +0330

    feat: Add Sections to models.py these sections were added: - About Me - Contact Info - Skills, Skill Level - Social Media - My works - Resume - Contact Me

--- kiarash/website/admin.py
+++ kiarash/website/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import AboutMe, ContactInfo, SkillLevel, Skills, SocialMedia, MyWorks, Resume, ContactMe
 
 # Register your models here.
+admin.site.register(AboutMe)
+admin.site.register(ContactInfo)
+admin.site.register(SkillLevel)
+admin.site.register(SocialMedia)
+admin.site.register(MyWorks)
+admin.site.register(Resume)
+
+
+@admin.register(Skills)
+class ModelDataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level')
+
+@admin.register(ContactMe)
+class ModelDataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email_address', 'subject', 'created_on')
--- kiarash/website/models.py
+++ kiarash/website/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField 
 
 # Create your models here.
+# home
+
+
+# About me
+class AboutMe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+#skill levels
+class SkillLevel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+#skills
+class Skills(models.Model):
+    name = models.CharField(max_length=255)
+    level = models.ForeignKey(SkillLevel, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+# my works
+class MyWorks(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    #add pic
+
+    def __str__(self) -> str:
+        return self.title
+    
+
+# resume
+class Resume(models.Model):
+    job_title = models.CharField(max_length=255)
+    start_from = models.DateField()
+    end_to = models.DateField(null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.job_title} - {self.start_from}"
+
+
+# contact info
+class ContactInfo(models.Model):
+    title = models.CharField(max_length=255)
+    phone_number = PhoneNumberField(null=True, blank=True, unique=True)
+    mail = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+# social media
+class SocialMedia(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+    display_order = models.SmallIntegerField(default=0)
+    # icon
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+# contact me
+class ContactMe(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    email_address = models.EmailField()
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
